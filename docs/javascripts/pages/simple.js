fb0899c58640ba93fbb79d23da57753b13145804
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-14 22:27:28 +0000

    Update docs/javascripts/pages/simple.js

--- docs/javascripts/pages/simple.js
+++ docs/javascripts/pages/simple.js
@@ -1,7 +1,7 @@
 const map = L.map('map').setView([39.74739, -105], 15);
 
 
-const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
