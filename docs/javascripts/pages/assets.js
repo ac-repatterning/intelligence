3aa17db45d26142fc5d6bae190c7154290d57246
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-14 22:57:33 +0000

    Update docs/javascripts/pages/assets.js

--- docs/javascripts/pages/assets.js
+++ docs/javascripts/pages/assets.js
@@ -1,4 +1,11 @@
 
+const map = L.map('map').setView([56.335, -4.103], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
 $.getJSON('../../warehouse/care_and_coarse_catchments.geojson', function (source){
 
 
