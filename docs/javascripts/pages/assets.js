7984d71fc87f01bf17d36cd1e3063a686c76203d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-31 05:15:56 +0000

    Update docs/javascripts/pages/assets.js: In Progress

--- docs/javascripts/pages/assets.js
+++ docs/javascripts/pages/assets.js
@@ -1,10 +1,19 @@
 
-const map = L.map('map').setView([56.335, -4.103], 7);
+const map = L.map('map').setView([58.786111, -4.114052], 8);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
+L.tileLayer('/{z}/{x}/{y}.png', {
+    attribution: '© Europa Technologies'
 }).addTo(map);
 
+/* On Map Click */
+var popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Point: " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
 
 $.getJSON('../../warehouse/care_and_coarse_catchments.geojson', function (source){
 
@@ -12,4 +21,3 @@ $.getJSON('../../warehouse/care_and_coarse_catchments.geojson', function (source
 
 
 })
-
